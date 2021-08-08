10359b482126148b48ba9881cf803aafcb1f41e2
Saurabh <saurabhkamboj1997@gmail.com>
2021-08-08 05:51:37 +0530

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
-export const API_URL = `https://vibeum.herokuapp.com`
+export const API_URL = `https://vibeum-app.herokuapp.com`
 export const CLOUDINARY_API_URL = `https://api.cloudinary.com/v1_1/saurabhkamboj`
